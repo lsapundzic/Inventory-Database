db9f0f09ef20401f5e6ea5acf1c99a01490917f6
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-07 20:47:25 +0100

    Connecting PHP components and debugging

---
+++ pages/index.php
@@ -0,0 +1,35 @@
+<?php
+
+// Header 
+include "./components/header.php"
+
+  ?>
+
+<body>
+  <!-- === === HEADER === === === -->
+  <header>
+    <img src="../images/colo-logo-medium.png" alt="COLO d.o.o. Company Logo" width="75" height="75" />
+    <h1>COLO Laboratory Experts</h1>
+    <h2>Inventory Management System</h2>
+  </header>
+
+  <!-- === === === NAVBAR === === === -->
+  <nav>
+    <div>Products</div>
+    <div>Stock</div>
+    <div>Arrivals</div>
+  </nav>
+
+  <!-- === === === MAIN === === === -->
+  <main></main>
+
+
+
+</body>
+
+<!-- === === === FOOTER === === === -->
+<?php
+include "./components/footer.php"
+  ?>
+
+</html>
