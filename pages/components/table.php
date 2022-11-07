71baecd8229400ca323ad103fa8b5646f5ab1884
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-07 22:05:28 +0100

    Table Template

---
+++ pages/components/table.php
@@ -0,0 +1,24 @@
+<table>
+    <tr>
+        <th>Row Description </th>
+        <td>Cell 1</td>
+        <td>Cell 2</td>
+        <td>Cell 3</td>
+        <td>Cell 4</td>
+    </tr>
+    <tr>
+        <th>Row Description </th>
+        <td>Cell 1</td>
+        <td colspan="3">Cell 2</td>
+        <td>Cell 3</td>
+        <td>Cell 4</td>
+    </tr>
+    <tr>
+        <th>Row Description </th>
+        <td>Cell 1</td>
+        <td>Cell 2</td>
+        <td>Cell 3</td>
+        <td>Cell 4</td>
+    </tr>
+
+</table>
