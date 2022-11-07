e4977eb925c682d83e57741395490d1ee9161fd5
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-07 21:33:46 +0100

    Lorem ipsum for main content

--- pages/components/footer.php
+++ pages/components/footer.php
@@ -2,6 +2,6 @@
     Steklarska ulica 1A <br>
     3250 Rogaska Slatina <br>
     Slovenia <br>
-    <a href="tel:+386 559 212">Tel. +386 559 212</a>
+    <a href="tel:+386 559 212">Tel. +386 559 212</a> <br>
     <a href="mailto: office@colo.si">office@colo.si</a>
 </address>
--- pages/index.php
+++ pages/index.php
@@ -13,7 +13,28 @@ include "./components/nav.php";
 <body>
 
   <main>
-    Welcome to the COLO Laboratory Experts Slovenia web page for trackign the shipments and cargo.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, similique! Blanditiis dolorum quae ex libero quidem
+    incidunt hic omnis nihil corporis fugit nemo consectetur voluptatibus, labore illo repellendus rem, doloribus
+    dolores unde dignissimos. Recusandae totam vero, aspernatur quaerat illum repudiandae, tempore doloribus minima
+    consectetur suscipit maxime similique accusamus? Architecto quas doloremque vero, deserunt officiis minima commodi!
+    Temporibus doloremque adipisci, rerum perferendis, fugiat nostrum molestias omnis quasi, magni perspiciatis dolorem
+    eligendi ratione. Quas facere omnis deleniti similique dolorem laborum inventore odit quaerat at enim cupiditate
+    nemo nulla rem sunt, iste voluptatum reiciendis sed? Reprehenderit debitis, explicabo eveniet quam eligendi neque
+    iusto fugiat repellat maiores necessitatibus architecto maxime ipsam accusantium consequatur quod natus rerum!
+    Accusamus tempore aspernatur et voluptatibus nam, at fugiat deleniti tenetur amet ab fugit, natus molestiae atque,
+    sed blanditiis eveniet numquam minus sit laboriosam commodi! Consequuntur animi, ab at, facilis odio odit inventore
+    maxime eum fugiat perspiciatis nesciunt in numquam quas saepe quisquam voluptates ducimus quis. Quod similique
+    incidunt qui sapiente. Tenetur earum a iusto natus tempore ab id exercitationem sed eligendi obcaecati. Eos neque
+    quos officia reiciendis, explicabo nisi facilis molestiae excepturi praesentium impedit dolorem asperiores quia
+    quisquam saepe distinctio ab, optio corrupti ut repellat eligendi tempore nostrum. Sapiente velit quo et, quidem,
+    itaque reiciendis autem veniam laborum accusamus provident, ipsa magnam. Minima distinctio totam eius debitis
+    repellat. Assumenda quos odio facilis? Ex repellendus eligendi iure in, asperiores quam, nam eum reiciendis
+    voluptatem expedita quisquam! Assumenda rem doloribus quia enim, molestiae magnam mollitia repellat quidem omnis
+    nihil eaque molestias quas distinctio incidunt harum libero eius porro pariatur, corporis ex, vero aspernatur.
+    Distinctio dolor recusandae perferendis ratione saepe, aut voluptatem quas accusamus quaerat obcaecati maiores
+    officia? Recusandae numquam veritatis assumenda enim minima nobis! Ex maxime ullam voluptatum veniam! Atque,
+    officiis labore repellat aliquid nobis quisquam qui quibusdam perspiciatis. Ipsa quos eos doloremque porro,
+    voluptates officia! Provident voluptas molest
   </main>
 
 </body>
