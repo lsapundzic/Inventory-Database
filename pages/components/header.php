4f0262d381dde8078e51549e4103938e0f00c052
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-07 20:47:22 +0100

    Connecting PHP components, debugging

---
+++ pages/components/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="real estate lower carniola showcase" />
+    <meta name="company name" content="Aera Real Estate d.o.o." />
+    <title>COLO Inventory Management Software</title>
+
+    <!-- CSS Style -->
+    <link rel="stylesheet" href="../styles/style.css" />
+
+    <!-- Script -->
+    <script src="../scripts/script.js" defer></script>
+
+</head>
