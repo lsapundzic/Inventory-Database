7c195b20da741d9e5dd495858ac3705552f231f4
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-09 18:21:15 +0100

    Footer bound to bottom

--- pages/components/footer.php
+++ pages/components/footer.php
@@ -25,12 +25,12 @@
         <a href="mailto: office@colo.si">office@colo.si</a>
 
     </div>
-
     <div class="location">
         <!-- <iframe class="gmap"
-        src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d11126.048487717624!2d15.1672567!3d45.8010011!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ssi!4v1590139467594!5m2!1sen!2ssi"
-        width="470" height="230" frameborder="0" style="border: 0;" allowfullscreen="" aria-hidden="false" tabindex="0">
-    </iframe> -->
+            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d11126.048487717624!2d15.1672567!3d45.8010011!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ssi!4v1590139467594!5m2!1sen!2ssi"
+            width="470" height="230" frameborder="0" style="border: 0;" allowfullscreen="" aria-hidden="false"
+            tabindex="0">
+        </iframe> -->
     </div>
 
 
