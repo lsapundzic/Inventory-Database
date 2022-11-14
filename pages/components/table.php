7e587b88978ad8e8d53158d61c276f1c52aefd29
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-14 17:21:18 +0100

    Put table into main

--- pages/components/table.php
+++ pages/components/table.php
@@ -1,27 +1,29 @@
-<div class="layout">
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>PRO</th>
-            <th>Name</th>
-            <th>Category</th>
-            <th>Supplier</th>
-            <th>Width</th>
-            <th>Height</th>
-            <th>Length</th>
-            <th>Weight</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>SKU331152</td>
-            <td>MICE8 Centrifuge</td>
-            <td>Centrifuge</td>
-            <td>Changsha Yingtai Centrifuge</td>
-            <td>25</td>
-            <td>30</td>
-            <td>35</td>
-            <td>8.5</td>
-        </tr>
+<main>
+    <div class="layout">
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>PRO</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Supplier</th>
+                <th>Width</th>
+                <th>Height</th>
+                <th>Length</th>
+                <th>Weight</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>SKU331152</td>
+                <td>MICE8 Centrifuge</td>
+                <td>Centrifuge</td>
+                <td>Changsha Yingtai Centrifuge</td>
+                <td>25</td>
+                <td>30</td>
+                <td>35</td>
+                <td>8.5</td>
+            </tr>
 
-    </table>
-</div>
+        </table>
+    </div>
+</main>
