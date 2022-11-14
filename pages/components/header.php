b1d865a4e71b1fd9f70e8d094fb940b373364820
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-14 16:34:52 +0100

    Better logo centering

--- pages/components/header.php
+++ pages/components/header.php
@@ -1,11 +1,12 @@
 <header>
-    <div>
-        <h1>COLO Lab Experts</h1>
-        <h2>Inventory Management Software</h2>
-    </div>
     <div class="logo">
         <a href="./index.php">
             <img src="../images/colo-logo-medium.png" alt="Company logo" width="40px" height="40px">
         </a>
     </div>
+    <div>
+        <h1>COLO Lab Experts</h1>
+        <h2>Inventory Management Software</h2>
+    </div>
+
 </header>
