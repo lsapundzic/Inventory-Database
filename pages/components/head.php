b612fdeae56af2d0adee378855b8f95d35f77d93
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-14 11:40:11 +0100

    Added favicon

--- pages/components/head.php
+++ pages/components/head.php
@@ -23,4 +23,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="../images/colo-logo-medium.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/colo-logo-medium.png" />
+
 </head>
