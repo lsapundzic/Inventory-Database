719db033bcfb76af0ac671ee0a46e17ca3ab1aac
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-14 16:58:42 +0100

    Inserted table into pages

--- pages/arrivals.php
+++ pages/arrivals.php
@@ -9,6 +9,9 @@ include "./components/header.php";
 // Navbar 
 include "./components/nav.php";
 
+// Table 
+include "./components/table.php";
+
 // Footer
 include "./components/footer.php";
 
--- pages/stock.php
+++ pages/stock.php
@@ -9,6 +9,8 @@ include "./components/header.php";
 // Navbar 
 include "./components/nav.php";
 
+// Table 
+include "./components/table.php";
 
 // Footer
 include "./components/footer.php";
