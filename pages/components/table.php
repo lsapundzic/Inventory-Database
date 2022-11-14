9a0fca18aee94413026dcd231c89d1ef9dc3c46b
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-14 17:26:19 +0100

    Table structure according to MDN

--- pages/components/table.php
+++ pages/components/table.php
@@ -1,105 +1,112 @@
 <main>
     <div class="layout">
         <table>
-            <tr>
-                <th>ID</th>
-                <th>PRO</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Supplier</th>
-                <th>Width</th>
-                <th>Height</th>
-                <th>Length</th>
-                <th>Weight</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
+            <caption>Table Displaying The Product Information</caption>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>PRO</th>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Supplier</th>
+                    <th>Width</th>
+                    <th>Height</th>
+                    <th>Length</th>
+                    <th>Weight</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>SKU331152</td>
+                    <td>MICE8 Centrifuge</td>
+                    <td>Centrifuge</td>
+                    <td>Changsha Yingtai Centrifuge</td>
+                    <td>25</td>
+                    <td>30</td>
+                    <td>35</td>
+                    <td>8.5</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SKU331152</td>
+                    <td>MICE8 Centrifuge</td>
+                    <td>Centrifuge</td>
+                    <td>Changsha Yingtai Centrifuge</td>
+                    <td>25</td>
+                    <td>30</td>
+                    <td>35</td>
+                    <td>8.5</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SKU331152</td>
+                    <td>MICE8 Centrifuge</td>
+                    <td>Centrifuge</td>
+                    <td>Changsha Yingtai Centrifuge</td>
+                    <td>25</td>
+                    <td>30</td>
+                    <td>35</td>
+                    <td>8.5</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SKU331152</td>
+                    <td>MICE8 Centrifuge</td>
+                    <td>Centrifuge</td>
+                    <td>Changsha Yingtai Centrifuge</td>
+                    <td>25</td>
+                    <td>30</td>
+                    <td>35</td>
+                    <td>8.5</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SKU331152</td>
+                    <td>MICE8 Centrifuge</td>
+                    <td>Centrifuge</td>
+                    <td>Changsha Yingtai Centrifuge</td>
+                    <td>25</td>
+                    <td>30</td>
+                    <td>35</td>
+                    <td>8.5</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SKU331152</td>
+                    <td>MICE8 Centrifuge</td>
+                    <td>Centrifuge</td>
+                    <td>Changsha Yingtai Centrifuge</td>
+                    <td>25</td>
+                    <td>30</td>
+                    <td>35</td>
+                    <td>8.5</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SKU331152</td>
+                    <td>MICE8 Centrifuge</td>
+                    <td>Centrifuge</td>
+                    <td>Changsha Yingtai Centrifuge</td>
+                    <td>25</td>
+                    <td>30</td>
+                    <td>35</td>
+                    <td>8.5</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>SKU331152</td>
+                    <td>MICE8 Centrifuge</td>
+                    <td>Centrifuge</td>
+                    <td>Changsha Yingtai Centrifuge</td>
+                    <td>25</td>
+                    <td>30</td>
+                    <td>35</td>
+                    <td>8.5</td>
+                </tr>
+            </tbody>
+
+
 
         </table>
     </div>
