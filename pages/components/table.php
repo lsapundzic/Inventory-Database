2f0f0f11835bd17e7aeb6f3f684d66eea3f46592
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-14 17:15:38 +0100

    Created a sample table

--- pages/components/table.php
+++ pages/components/table.php
@@ -1,25 +1,26 @@
 <div class="layout">
     <table>
         <tr>
-            <th>Row Description </th>
-            <td>Cell 1</td>
-            <td>Cell 2</td>
-            <td>Cell 3</td>
-            <td>Cell 4</td>
+            <th>ID</th>
+            <th>PRO</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Supplier</th>
+            <th>Width</th>
+            <th>Height</th>
+            <th>Length</th>
+            <th>Weight</th>
         </tr>
         <tr>
-            <th>Row Description </th>
-            <td>Cell 1</td>
-            <td colspan="3">Cell 2</td>
-            <td>Cell 3</td>
-            <td>Cell 4</td>
-        </tr>
-        <tr>
-            <th>Row Description </th>
-            <td>Cell 1</td>
-            <td>Cell 2</td>
-            <td>Cell 3</td>
-            <td>Cell 4</td>
+            <td>1</td>
+            <td>SKU331152</td>
+            <td>MICE8 Centrifuge</td>
+            <td>Centrifuge</td>
+            <td>Changsha Yingtai Centrifuge</td>
+            <td>25</td>
+            <td>30</td>
+            <td>35</td>
+            <td>8.5</td>
         </tr>
 
     </table>
