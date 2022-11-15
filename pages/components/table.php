e573e1fc36f6f06892cac70f55e98f8cce3390dc
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-15 17:44:22 +0100

    Rollback

--- pages/components/table.php
+++ pages/components/table.php
@@ -15,9 +15,6 @@ include_once "../database/connect.php";
     // Check if the connection actually returns anything 
     $resultCheck = mysqli_num_rows($result);
 
-    // What works? 
-    echo "<div>{$conn} / {$result} / {$resultCheck}</div>";
-
     // Test if this works 
     if ($resultCheck > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
