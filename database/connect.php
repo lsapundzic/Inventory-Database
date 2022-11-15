b98d51fe7363d0fd68892825c03017cde82cd9b7
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-15 19:25:34 +0100

    changed variable name

--- database/connect.php
+++ database/connect.php
@@ -5,9 +5,9 @@ $db = 'logistics';
 $host = 'localhost';
 $port = 3306;
 
-$link = mysqli_init();
+$conn = mysqli_init();
 $success = mysqli_real_connect(
-    $link,
+    $conn,
     $host,
     $user,
     $password,
--- database/connecttwo.php
+++ database/connecttwo.php
@@ -4,10 +4,10 @@ $username = "root";
 $password = "root";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$connn = new mysqli($servername, $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
+if ($connn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
     echo "Connected successfully from connect.php";
