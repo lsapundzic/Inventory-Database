ecaca0d2964cdc7613960794eeaeaccc4004bd32
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-18 19:56:13 +0100

    First attempt at how this should look like

--- pages/components/table.php
+++ pages/components/table.php
@@ -2,64 +2,68 @@
 include_once "../database/connect.php";
 ?>
 
-
 <?php
 
-    try {
-        // Query that I want to be displayed 
-        $sql = "SELECT * FROM contact";
+try {
+    // Query that I want to be displayed 
+    $sql = "SELECT * FROM contact";
 
-        // Establish a connection with the database and execute the search query 
-        $result = mysqli_query($conn, $sql);
+    // Establish a connection with the database and execute the search query 
+    $result = mysqli_query($conn, $sql);
 
-        // Check if the connection actually returns anything 
-        $resultCheck = mysqli_num_rows($result);
+    // Check if the connection actually returns anything 
+    $resultCheck = mysqli_num_rows($result);
 
-        // Test if this works 
-        if ($resultCheck > 0) {
+    // Test if this works 
+    if ($resultCheck > 0) {
 
-            // Display table head
+        // Display table head
+        echo "
             <main>
-            while ($row = mysqli_fetch_assoc($result)) {
-                // echo "<div>{$row['name']}</div>";
-    
-                
-                <table>
-                <caption>Table Displaying The Product Information</caption>
-                <thead>
-                    <tr>
-                        <th>PRO</th>
-                        <th>Name</th>
-                        <th>Category</th>
-                        <th>Supplier</th>
-                        <th>Width</th>
-                        <th>Height</th>
-                        <th>Length</th>
-                        <th>Weight</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>SKU331152</td>
-                        <td>MICE8 Centrifuge</td>
-                        <td>Centrifuge</td>
-                        <td>Changsha Yingtai Centrifuge</td>
-                        <td>25</td>
-                        <td>30</td>
-                        <td>35</td>
-                        <td>8.5</td>
-                    </tr>
-                </tbody>
-            </table>
+            <table>
+            <caption>Table Displaying The Product Information</caption>
+            <thead>
+                <tr>
+                    <th>PRO</th>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Supplier</th>
+                    <th>Width</th>
+                    <th>Height</th>
+                    <th>Length</th>
+                    <th>Weight</th>
+                </tr>
+            </thead>
+            <tbody> ";
 
-            }
+        while ($row = mysqli_fetch_assoc($result)) {
+            // echo "<div>{$row['name']}</div>";
 
-            </main>
+            echo "
+                <tr>
+                    <td>{$row['pro']}</td>
+                    <td>{$row['name']}</td>
+                    <td>{$row['category']}</td>
+                    <td>{$row['supplier']}</td>
+                    <td>{$row['width']}</td>
+                    <td>{$row['height']}</td>
+                    <td>{$row['length']}</td>
+                    <td>{$row['width']}</td>
+                </tr>  ";
+
         }
-    } catch (\Throwable $th) {
-        echo "Unable to display database info. Caught exception in table.php";
+
+        echo "
+            </tbody>
+            </table>
+            </main> ";
+    } else {
+        echo "Error: the table is probably empty";
     }
 
+} catch (\Throwable $th) {
+    echo "Unable to display database info. Caught exception in table.php";
+}
 
-    ?>
+
+?>
