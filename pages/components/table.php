1767f16ed17ed5a98afedca6f5a9f8343c0c4c7c
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-11-18 19:40:26 +0100

    Moved main into PHP

--- pages/components/table.php
+++ pages/components/table.php
@@ -2,8 +2,8 @@
 include_once "../database/connect.php";
 ?>
 
-<main>
-    <?php
+
+<?php
 
     try {
         // Query that I want to be displayed 
@@ -17,9 +17,45 @@ include_once "../database/connect.php";
 
         // Test if this works 
         if ($resultCheck > 0) {
+
+            // Display table head
+            <main>
             while ($row = mysqli_fetch_assoc($result)) {
-                echo "<div>{$row['name']}</div>";
+                // echo "<div>{$row['name']}</div>";
+    
+                
+                <table>
+                <caption>Table Displaying The Product Information</caption>
+                <thead>
+                    <tr>
+                        <th>PRO</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Supplier</th>
+                        <th>Width</th>
+                        <th>Height</th>
+                        <th>Length</th>
+                        <th>Weight</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>SKU331152</td>
+                        <td>MICE8 Centrifuge</td>
+                        <td>Centrifuge</td>
+                        <td>Changsha Yingtai Centrifuge</td>
+                        <td>25</td>
+                        <td>30</td>
+                        <td>35</td>
+                        <td>8.5</td>
+                    </tr>
+                </tbody>
+            </table>
+
             }
+
+            </main>
         }
     } catch (\Throwable $th) {
         echo "Unable to display database info. Caught exception in table.php";
@@ -27,34 +63,3 @@ include_once "../database/connect.php";
 
 
     ?>
-
-    <table>
-        <caption>Table Displaying The Product Information</caption>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>PRO</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Supplier</th>
-                <th>Width</th>
-                <th>Height</th>
-                <th>Length</th>
-                <th>Weight</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>SKU331152</td>
-                <td>MICE8 Centrifuge</td>
-                <td>Centrifuge</td>
-                <td>Changsha Yingtai Centrifuge</td>
-                <td>25</td>
-                <td>30</td>
-                <td>35</td>
-                <td>8.5</td>
-            </tr>
-        </tbody>
-    </table>
-</main>
